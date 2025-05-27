db20b3d0ab45e96d9cedd3c26b32c77b45041d16
Thomas <thomas@socraft.ch>
2025-05-27 19:02:53 +0200

    chore: modify logic server

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import os
-
 from flask import Flask, jsonify, request
-
 from generate_commit import generate_commit_messages
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
@@ -9,14 +7,12 @@ os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "0"
 
 app = Flask(__name__)
 
-
 @app.route("/suggest", methods=["POST"])
 def suggest():
     data = request.get_json()
-    diff_clean = data["diff"]
+    diff_clean = data.get("diff", "")
     messages = generate_commit_messages(diff_clean)
     return jsonify({"suggestions": messages})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
